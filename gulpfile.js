6358d248761a2d3b9632154195346ab647cd75c1
Tim Zschage <Tim Zschage@united-it.net>
2016-11-07 11:55:42 +0100

    Test commit

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+gulp.task('styles', function() {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css/'));
+});
+
+gulp.task('default',function(){
+  gulp.watch('sass/**/*.scss',['styles']);
+});
+
+
+gulp.task('compress', function () {
+  gulp.src('js/*.js')
+  .pipe(minify({
+      ext:{
+          src:'-debug.js',
+          min:'.js'
+      },
+      exclude: ['tasks'],
+      ignoreFiles: ['.combo.js', '-min.js']
+  }))
+  .pipe(gulp.dest('dist'))
+});
